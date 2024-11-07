d1ee6cb7f7ab71dbcb00f7e4a4c047c0e158837b
Ilja Grigorjev <tomkapisos@gmail.com>
2024-11-07 15:44:59 +0100

    Utils Test

---
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UtilsTest.java
@@ -0,0 +1,41 @@
+package se.yrgo.libraryapp.validators;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilsTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "'Hello World', 'hello world'",
+            "'Hello World123', 'hello world'",
+            "'Hello!@#$%^&*World', 'helloworld'",
+            "'Hello1    World2  Test3', 'hello    world  test'",
+            "'', ''" // empty string case
+    })
+    void testOnlyLettersAndWhitespaces(String text, String expected) {
+        assertEquals(expected, Utils.onlyLettersAndWhitespace(text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'Us3r', 'user'",
+            "'H3ll0 W0rld', 'hello world'",
+            "'J4v4   14ng', 'java   lang'",
+            "'*/+H3llo@#$%^&()#_!+<>,', 'hello'",
+            "'*****', ''",
+            "'', ''",
+            "'   ', '   '",
+            "'Hello1 23World 45', 'hellol eworld as'",
+            "'L337 C0d3', 'leet code'",
+            "'b4d c0de @#$%', 'bad code '"
+    })
+    void testCleanAndUnLeet(String text, String expected) {
+        assertEquals(expected, Utils.cleanAndUnLeet(text));
+    }
+}
+
